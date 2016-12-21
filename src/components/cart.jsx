a13c05d076dcc0dd78df2c211cd5251de18fad72
ashish sinha <ashishsinha14@gmail.com>
2016-12-21 15:01:26 -0500

    Added Cart

---
+++ src/components/cart.jsx
@@ -0,0 +1,61 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ShoppingCart extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            qty: props.initialQty,
+            total: 0
+        };
+    }
+    componentWillMount(){
+        this.recalculateTotal();
+    }
+    increaseQty(){
+        this.setState({qty:this.state.qty+1},this.recalculateTotal);
+    }
+    decreaseQty(){
+        let newQty = this.state.qty > 0 ? this.state.qty -1 : 0;
+        this.setState({qty:newQty},this.recalculateTotal);
+    }
+    recalculateTotal(){
+        this.setState({total:this.state.qty * this.props.price});
+    }
+    render() {
+        return <article>
+            <figure>
+                <p>
+                    <img src={this.props.image}/>
+                </p>
+                <figcaption>
+                    <h2>{this.props.title}</h2>
+                </figcaption>
+            </figure>
+            <p><strong>Quantity: {this.state.qty}</strong></p>
+
+            <p>
+                <button onClick={this.increaseQty.bind(this)} className="button success">+</button>
+                <button onClick={this.decreaseQty.bind(this)} className="button alert">-</button>
+            </p>
+
+            <p><strong>Price per item:</strong> ${this.props.price}</p>
+
+            <h3>
+            Total: ${this.state.total}
+        </h3>
+
+    </article>;
+    }
+}
+
+ShoppingCart.propTypes = {
+    title: React.PropTypes.string.isRequired,
+    price: React.PropTypes.number.isRequired,
+    initialQty: React.PropTypes.number
+};
+ShoppingCart.defaultProps = {
+    title: 'Undefined Product',
+    price: 100,
+    initialQty: 0
+};
---
+++ src/components/searchcontrol.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class SearchControl extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    }
+    render(){
+        return();
+    }
+}
